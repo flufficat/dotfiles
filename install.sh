e01c1476a209f292296a5e597c49cf0efda4f99b
flufficat <112202351+flufficat@users.noreply.github.com>
2023-03-25 12:15:01 +0000

    added first installer barebones

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+ ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+ ░█▀▀░█░░░█░█░█▀▀░█▀▀░▀█▀░█▀▀░█▀█░▀█▀░
+ ░█▀▀░█░░░█░█░█▀▀░█▀▀░░█░░█░░░█▀█░░█░░
+ ░▀░░░▀▀▀░▀▀▀░▀░░░▀░░░▀▀▀░▀▀▀░▀░▀░░▀░░
+ ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+ ░░░ github.com/flufficat/dotfiles ░░░
+ ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+
+## Repo apps
+# python-pywal liferea qbittorent brave-bin alacritty darktable fish gh mpv mullvad-vpn-bin nicotine+ neovim onlyoffice-bin stacer tlpui autotiling anki-bin appimagelauncher rmtrash exa python-pip
+
+## Appimages
+# simplenote bitwarden 
+
+## Python packages
+# youtube-dl
+
+
+## Install
+sudo pacman --noconfirm -Syu --needed yay
+yay --noconfirm -S 
