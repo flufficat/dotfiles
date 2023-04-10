bab8f61972dcbd8419bcc3cfb7b16c766f8f6f40
flufficat <112202351+flufficat@users.noreply.github.com>
2023-04-10 20:25:06 +0100

    removed some video filters

--- .config/sway/scripts/lockman.sh
+++ .config/sway/scripts/lockman.sh
@@ -4,6 +4,7 @@ swayidle \
     timeout  300 'swaymsg "output * power off"' \
     resume 'swaymsg "output * power on"' &
 # Locks the screen immediately
-swaylock
+swaylock 
+
 # Kills last background task so idle timer doesn't keep running
 kill %%
